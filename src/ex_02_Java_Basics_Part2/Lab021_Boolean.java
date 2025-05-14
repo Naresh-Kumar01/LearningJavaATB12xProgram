778f433dcac961b3249a586eabcc77f575545573
Naresh-kumar01 <Nareshsofttechh@gmail.com>
2025-05-14 19:53:44 +0530

    11th may 2025

---
+++ src/ex_02_Java_Basics_Part2/Lab021_Boolean.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab021_Boolean {
+    public static void main(String[] args) {
+  boolean is_naresh_married=true;
+  boolean nafresh_have_it_job=false;
+  boolean is_this_possible_good_things_required_times=true;
+    }
+
+}
