87a1cba52762791414905a2f54494691408db111
Naresh-kumar01 <Nareshsofttechh@gmail.com>
2025-05-25 11:47:34 +0530

    25th may 2025 p2

---
+++ src/ex_14_Strings/Lab136_String_Creation.java
@@ -0,0 +1,7 @@
+package ex_14_Strings;
+
+public class Lab136_String_Creation {
+    String s1 = "pramod"; // SCP -> string constant pool
+
+    String s2 = new String("pramod"); // OA - Object area
+}
